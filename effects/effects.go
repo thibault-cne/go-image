f8c207f3f60b5664c33bddbb7a43c174a656b37e
Thib <thibault.cheneviere@telecomnancy.net>
2022-09-28 11:59:07 +0200

    [ADD] Add the ColorFilter effect to filter some colors on a picture. The pixels filtered are rendered in grayscale.

--- effects/effects.go
+++ effects/effects.go
@@ -2,6 +2,7 @@ package effects
 
 import (
 	"go-image/clone"
+	"go-image/kolor"
 	"go-image/util"
 	"image"
 	"image/color"
@@ -99,6 +100,8 @@ func Invert(img image.Image, config ...int) *image.RGBA {
 	return dst
 }
 
+// Function to use Sobal edge detection. It returns a grayscale image with edges of the picture in white.
+// See https://en.wikipedia.org/wiki/Sobel_operator
 func SobalEdge(img image.Image) *image.Gray {
 	src := clone.CloneAsRGBA(img)
 	bounds := src.Bounds()
@@ -149,6 +152,9 @@ func SobalEdge(img image.Image) *image.Gray {
 	return dst
 }
 
+// This function is for gaussian blur.
+//
+//	radius : the gaussian matrix radius (1 render a 3x3 matrix)
 func GaussianBlur(img image.Image, radius int) *image.RGBA {
 	src := clone.CloneAsRGBA(img)
 	bounds := src.Bounds()
@@ -172,6 +178,42 @@ func GaussianBlur(img image.Image, radius int) *image.RGBA {
 				}
 			}
 			dst.Set(x, y, color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)})
+		}
+	}
+
+	return dst
+}
+
+// We keep only pix close to a certain color of a distance in the L*a*b* color representation.
+// Other pixels are greyscaled
+//
+//	c : the color you want to keep in the image
+//	d : the maximal distance to the color you want to keep
+func ColorFilter(img image.Image, c color.RGBA, d float64) *image.RGBA {
+	// Set the weight for the grayscale luma method. By default it respects the ITU-R recommendations
+	r := 0.2126
+	g := 0.7152
+	b := 0.0722
+
+	src := clone.CloneAsRGBA(img)
+	bounds := src.Bounds()
+	w, h := bounds.Dx(), bounds.Dy()
+
+	dst := image.NewRGBA(bounds)
+
+	k, _ := kolor.MakeKolor(c)
+
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			pix := src.At(x, y).(color.RGBA)
+			p, _ := kolor.MakeKolor(pix)
+
+			if k.DistanceLAB(p) < d {
+				dst.Set(x, y, color.RGBA{R: pix.R, G: pix.G, B: pix.B, A: pix.A})
+			} else {
+				gray := uint8(float64(pix.R)*r + float64(pix.G)*g + float64(pix.B)*b + 0.5)
+				dst.Set(x, y, color.RGBA{gray, gray, gray, pix.A})
+			}
 		}
 	}
 
